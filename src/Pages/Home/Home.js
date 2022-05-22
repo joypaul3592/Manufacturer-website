57e6768fddde5c60e6bb2d7ad68d37ada1cac7f5
Joy Paul <joypaul3592@gmail.com>
2022-05-22 10:17:11 +0600

    adding products section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../Sheard/Navbar';
 import Header from './Header';
+import Products from './Products';
 
 const Home = () => {
     return (
         <div>
 
             <Header></Header>
+            <Products></Products>
         </div>
     );
 };
